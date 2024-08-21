ae3f7a6e999f60713e08f97db37f8a61e73cc3be
bobrobotsun <bobrobotsun@163.com>
2024-08-22 01:36:25 +0800

    2024.08.21.01-movoid dev 可以触发github test

---
+++ test/test_timer.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+# File          : test_timer
+# Author        : Sun YiFan-Movoid
+# Time          : 2024/8/22 1:35
+# Description   : 
+"""
+from movoid_timer import Timer
+
+
+class TestTimer:
+    def test_timer(self):
+        timer = Timer()
